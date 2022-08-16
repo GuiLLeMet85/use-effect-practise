e15a4c1ff6e0e4b42d03860e04ccfa63a57d7d4b
Guillem B <guillembaracco@gmail.com>
2022-08-16 21:18:50 +0200

    iteration 2 done

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,6 +5,17 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   const [dynamicClass, setDynamicClass] = useState('text');
 
+ 
+  useEffect(() => {
+    setCount(Math.floor(Math.random() * (10 - 0) + 0));
+
+    return () => {
+      setCount(0);
+      setDynamicClass('text');
+    }
+  }, [])
+
+
 
   const handleIncrease = () => {
       setCount(value => value +1)
