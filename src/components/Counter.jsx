f7c9d75d4846dfd9b7a316fb2ef6843df6c70305
Guillem B <guillembaracco@gmail.com>
2022-08-17 01:47:30 +0200

    iteration 3 done

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,10 +5,9 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   const [dynamicClass, setDynamicClass] = useState('text');
 
- 
+  // Second and Fourth iteration
   useEffect(() => {
     setCount(Math.floor(Math.random() * (10 - 0) + 0));
-
     return () => {
       setCount(0);
       setDynamicClass('text');
@@ -16,7 +15,7 @@ export default function Counter() {
   }, [])
 
 
-
+  // First iteration
   const handleIncrease = () => {
       setCount(value => value +1)
   }
@@ -25,11 +24,19 @@ export default function Counter() {
     setCount(value => value -1)
   }
 
-  // First iteration
-
-  // Second and Fourth iteration
-
   // Third iteration
+
+useEffect (() => {
+  if (count <= 0) {
+  setDynamicClass('red');
+  }
+  else if (count > 0 && count <= 5) {
+    setDynamicClass('orange');
+  } 
+  else if (count > 5 ) {
+    setDynamicClass ('green');
+  }
+}, [count])
 
   return (
     <div>
